005b35f4cc33dc881a08e7149f57c683782477b0
Luca <129970531+omGalle@users.noreply.github.com>
2023-10-05 16:08:45 +0200

    S2L4.c

--- S2L4.c
+++ S2L4.c
@@ -5,7 +5,7 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
+// PROGRAMMA GIOCO A DOMANDE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
